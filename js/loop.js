3c5a720f1fe5de589672f82f8db839c656ad9768
inagaki <h.inagaki@hiyoku.co.jp>
2024-02-05 17:23:41 +0900

    while文では無限ループに注意する

--- js/loop.js
+++ js/loop.js
@@ -12,3 +12,18 @@ do {
   i += 1;
   console.log(i);
 } while (i < 10);
+
+
+// while
+// 条件がtrueに評価される限り文を実行する。
+/**
+  while (条件式)
+  文
+*/
+let n = 0;
+let x = 0;
+while (n < 3) {
+  n++;
+  x += n;
+  console.log(x);
+}
